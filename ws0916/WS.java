919489e772e693d791d413db76f5cf70e50488f5
kimjungwon <koo_m@naver.com>
2019-09-16 06:19:04 +0000

    Update ws0916/WS.java

---
+++ ws0916/WS.java
@@ -0,0 +1,30 @@
+﻿package test;
+
+public class solution {
+	static int [] coin = {1,4,6}; 
+	static int min; 
+	public static void main(String[] args) 
+	{
+		int money = 3; 
+		min = Integer.MAX_VALUE; 
+		makeChange(money, 0);
+	}
+	static void makeChange( int money, int cnt)
+	{
+		for(int i=0; i< coin.length; i++)
+		{
+			if(money == coin[i])
+			{
+				if( min > cnt +1)
+				{
+					min = cnt +1;
+				}
+				return ;
+			}
+			else if( money> coin[i])
+			{
+				makeChange(money-coin[i] , cnt+1);
+			}
+		}
+	}
+}
