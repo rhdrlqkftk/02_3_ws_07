2ade6c2a286967b88fd353157ffbb975c68fcab5
kimjungwon <koo_m@naver.com>
2019-09-23 07:58:01 +0000

    Update ww0923/수진이의 팰린드롬.java

---
+++ ww0923/수진이의 팰린드롬.java
@@ -0,0 +1,34 @@
+﻿package SWEX;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static char[] master;
+	public static void main(String[] args) throws Exception, IOException 
+	{
+		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine()); 
+		for(int t =1 ; t<=T; t++)
+		{	
+			String s = new String(br.readLine());
+			int arr[] = new int[26];
+			int ans =0; 
+			
+			for(int i=0 ;i <s.length(); i++ )
+			{
+				arr[s.charAt(i)-'a']++;
+			}
+			for(int i =0; i < 26; i++)
+			{
+				ans  += (arr[i]*(arr[i]+1) /2);
+			}
+			System.out.println("#"+ t + " "+ ans);
+		}
+		
+	}
+}
