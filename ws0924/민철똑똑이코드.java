5258cc7e5e94258f0eaec25af5d6ad8ad9068700
kimjungwon <koo_m@naver.com>
2019-09-24 06:19:43 +0000

    Update ws0924/민철똑똑이코드.java

---
+++ ws0924/민철똑똑이코드.java
@@ -0,0 +1,36 @@
+﻿import java.util.Scanner;
+
+public class Solution3 {
+	static int N; 
+	static int Map[]; 
+	
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int Tc = sc.nextInt(); 
+		for(int t =1; t <= Tc; t++)
+		{
+			N = sc.nextInt();
+			Map = new int[N];
+			for(int i =0 ; i< N ;i++)
+			{				
+				Map[i] =sc.nextInt(); 
+				
+			}
+			int idx =0, ans =0; 
+			while( idx +1 <N )
+			{
+				int a =0; 
+				while( idx +1 < N && Map[idx] <Map[idx+1])
+				{
+					a++; idx++; 
+				}
+				while( idx +1 < N && Map[idx] >Map[idx+1])
+				{
+					ans += a; idx++; 
+				}
+			}
+			System.out.println("#" +t + " " + ans);
+		}
+	}
+}
