10c8998f0e277db7ca812dc29eeca4b3e292268a
kimjungwon <koo_m@naver.com>
2019-09-24 05:50:35 +0000

    Update ws0924/정식이의 은행업무.java, ws0924/콩 많이 심기.java files

---
+++ ws0924/정식이의 은행업무.java
@@ -0,0 +1,100 @@
+﻿package SWEX;
+
+import java.util.Scanner;
+
+public class SWHW {
+	static int[] master;
+	static int[] master1;
+	static long ans=0;
+	static boolean chk =false; 
+	static void solve()
+	{
+		long d2 =0; 
+		long d3 =0; 
+		for(int i =0; i< master.length; i++)
+		{
+			d2 *=2; 
+			d2 += master[i];
+		}
+		for(int i =0; i< master1.length; i++)
+		{
+			d3 *=3; 
+			d3 += master1[i];
+		}
+		if( d2==d3)
+		{
+			chk = true; 
+			ans = d2; 
+		}
+		
+		return ; 
+	}
+	static void fs3()
+	{
+		for( int j =0 ;j < master1.length; j++)
+		{
+			for(int k =0; k<3; k++)
+			{
+				if(master1[j]== k) continue;
+				int tmp = master1[j]; 
+				master1[j]= k;
+				solve(); 
+				if( chk == true)
+				{
+					return;
+				}
+				master1[j]= tmp; 
+			}
+		}
+	}
+	static void fs2()
+	{
+		for(int i=0;  i<master.length; i++)
+		{
+			if( master[i] == 1)
+			{
+				master[i] = 0; 
+			}
+			else 
+			{
+				master[i] = 1;
+			}
+			fs3(); 
+			if( chk == true)
+			{
+				return;
+			}
+			if( master[i] == 1)
+			{
+				master[i] = 0; 
+			}
+			else 
+			{
+				master[i] = 1;
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int Tc = sc.nextInt();
+		for(int t = 1; t <= Tc; t++)
+		{
+			chk = false; 
+			String s = sc.next();
+			master = new int[s.length()];
+			for(int i=0 ; i< s.length(); i++)
+			{
+				master[i] =  Integer.parseInt(s.charAt(i)+ "");			
+			}
+			s = sc.next();
+			master1 = new int[s.length()];
+			for(int i=0 ; i< s.length(); i++)
+			{
+				master1[i] = Integer.parseInt(s.charAt(i)+ "");			
+			}
+			fs2();
+			System.out.println("#" + t+ " "+ ans);
+			
+		}
+	}
+}
---
+++ ws0924/콩 많이 심기.java
@@ -0,0 +1,49 @@
+﻿import java.util.Scanner;
+
+public class Solution {
+	static int n; 
+	static int m; 
+	static boolean Map[][]; 
+	static boolean boundary(int x, int y)
+	{
+		if(x <0 || y <0 || x >= m || y >= n)
+		{
+			return false; 
+		}
+		return true; 
+	}
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int Tc = sc.nextInt(); 
+		for(int t =1; t <= Tc; t++)
+		{
+			n = sc.nextInt();  // 가로
+			m = sc.nextInt();  // 세로
+			int ans =0; 
+			for(int i =0; i <n; i++)
+			{
+				for(int j =0 ;j <m;j++)
+				{
+					if( i %4 <2)
+					{
+						if( j %4 <2)
+						{
+							ans ++; 							
+						}
+					}
+					else 
+					{
+						if( j % 4 >=2)
+						{
+							ans ++;
+						}
+					}
+				}
+			}
+			System.out.println("#" + t+ " "+ ans);
+		}
+	}
+}
+//https://swexpertacademy.com/main/talk/solvingClub/problemView.do?contestProbId=AWLv-yZah48DFAVV&solveclubId=AWv0G8c6S0wDFASy&problemBoxTitle=%EC%9B%8C%ED%81%AC%EC%83%B5+0924&problemBoxCnt=1&probBoxId=AW1gpNJK9wMDFARC
+// 왜 이생각을 못했을까... 자괴감이 느껴지는 문제 
