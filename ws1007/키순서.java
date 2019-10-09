06890935c81ba5100ec47007ae2e7e5e4e04766d
kimjungwon <koo_m@naver.com>
2019-10-09 08:10:16 +0000

    Update ws1007/키순서.java

--- ws1007/키순서.java
+++ ws1007/키순서.java
@@ -1,72 +1,86 @@
-package ssafy1007;
-import java.util.Scanner;
-// 해쉬맵 
-// dfs
-// 플로이드 와샬 
-public class 키순서 {
-	static int map[][] ;
-	static int count[]; 
-	static boolean visit[]; 
-	static int N, M; 
-	static void dfs_up(int start, int curr)
-	{
-		for(int i =0; i < N; i++)
-		{
-			if(map[curr][i] == 1 && !visit[i])
-			{
-				visit[i] = true; 
-				count[start]++; 
-				dfs_up(start, i);
-			}
-		}
-	}
-	static void dfs_down(int start, int curr)
-	{
-		for(int i =0; i < N; i++)
-		{
-			if(map[i][curr] == 1 && !visit[i])
-			{
-				visit[i] = true; 
-				count[start]++; 
-				dfs_down(start, i);
-			}
-		}
-	}
-	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-		int tc =sc.nextInt(); 
-		for(int T = 1; T<=tc; T++)
-		{
-			N = sc.nextInt(); // 학생수 
-			M = sc.nextInt(); // 비교 횟수.
+package ws1009;
 
-			map = new int[N][N]; // 순위.
-			count = new int[N];
-			
-			for(int i=0;i < M; i++) // 앞에가 뒤에 보다 작다는 것을 의미한다. 
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 키순서 {
+	// 플로이드 와샬로 해결한다면 ~? 
+	static int N,M, Map[][],Map1[][]; 
+	static int chk[], chk1[];
+	static boolean ch[]; 
+	static boolean ch1[]; 
+	// 두번 겹치는경우는 빼줘야할 꺼 같은데 흠.. 
+	static void dfs_up ( int origin, int start)
+	{
+		for(int i =1 ; i <=N; i++)
+		{
+			if( Map[start][i] ==1) // 작은경우
+			{
+				if( ch[i] == false)
+				{
+					chk[origin]++;
+					ch[i] = true;
+					dfs_up(origin, i);					
+				}
+			}
+		}
+		return ; 
+	}
+	static void dfs_down (int origin, int start)
+	{
+		for(int i =1 ; i <=N; i++)
+		{
+			if( Map1[start][i] ==1) //큰경우.
+			{
+				if( ch1[i] == false)
+				{
+					ch1[i]= true;
+					chk1[origin]++;
+					dfs_down(origin, i);
+				}
+			}
+		}
+		return ; 
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt(); 
+		for(int tc = 1 ; tc<= T; tc++)
+		{
+			N = sc.nextInt();  // 사람수
+			M = sc.nextInt();  // 비교 횟수를 의미한다. 
+			Map  = new int[N+1][N+1];
+			Map1  = new int[N+1][N+1];
+			chk  = new int [N+1]; 
+			chk1 = new int [N+1];
+			ch = new boolean[N+1];
+			ch1 = new boolean[N+1];
+			for(int i =0 ; i <M;i++)
 			{
 				int a = sc.nextInt(); 
-				int b = sc.nextInt();
-				map[a][b] = 1; 
+				int b = sc.nextInt(); 
+				Map[a][b] =1; // a< b 인경우.
+				Map1[b][a] =1; // 앞에 꺼가 더 큰 경우. 
 			}
-			
-			for(int i =0; i< N; i++)
+			for(int i =1; i<= N; i++)
 			{
-				visit = new boolean[N];
-				dfs_up(i, 0);
-				visit = new boolean[N];
-				dfs_down(i,0);
+				Arrays.fill(ch, false);
+				Arrays.fill(ch1, false);
+				dfs_up( i,i);
+				dfs_down( i,i);
 			}
-			for(int i =0 ; i< M; i++)
+			int cnt=0;
+			for(int i=1; i <= N; i++)
 			{
-				
+				int k = chk[i] + chk1[i];
+				if(k == N-1)
+				{
+					cnt++;
+				}
 			}
-			System.out.println("#"+T + " " + 1);
+			System.out.println("#"+ tc+ " " + cnt);
 		}
-		
 	}
-
 }
 /*
 ● 1번 학생의 키 < 5번 학생의 키
@@ -74,6 +88,4 @@ public class 키순서 {
 ● 5번 학생의 키 < 4번 학생의 키
 ● 4번 학생의 키 < 2번 학생의 키
 ● 4번 학생의 키 < 6번 학생의 키
-● 5번 학생의 키 < 2번 학생의 키
-*/
-//w 자신의 키를 알수 있는 학생은 몇명인지...? 흠... 
+● 5번 학생의 키 < 2번 학생의 키*/
